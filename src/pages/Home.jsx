76c00a94d4e359d133259cf485db86107197179f
hilayo <hilalosh10@gmail.com>
2023-11-20 11:36:00 +0200

    github_founder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResult from "../component/users/UserResult";
+import UserSearch from "../component/users/UserSearch";
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResult />
+    </>
+  );
+}
+
+export default Home;
